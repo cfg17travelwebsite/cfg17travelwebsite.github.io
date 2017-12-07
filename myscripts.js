441b26ddf01ed8db59f7f0957577b1f54515af07
indiaabbott <32935927+indiaabbott@users.noreply.github.com>
2017-12-07 13:08:17 +0000

    improving
    just putting small details in

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,12 @@
-$("button").on("click", function() {alert("clicked!")});
+$(function () {
+    $("#dialog").dialog({
+        modal: true,
+        autoOpen: false,
+        title: "jQuery Dialog",
+        width: 300,
+        height: 150
+    });
+    $("#btnShow").click(function () {
+        $('#dialog').dialog('open');
+    });
+});
